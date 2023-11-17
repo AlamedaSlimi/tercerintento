dbbd73206ed2cae6e96b6df4af934bd4a206b540
AlamedaSlimi <89815816+AlamedaSlimi@users.noreply.github.com>
2023-11-16 23:41:34 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     const checkLogin = async () => {
       const token = localStorage.getItem('tokenStore')
       if (token) {
-        const verified = await axios.get('/user/verify', {
+        const verified = await axios.get('https://notesappla.onrender.com/user/verify', {
           headers: { Authorization: token }
         })
         setIsLogin(verified.data)
